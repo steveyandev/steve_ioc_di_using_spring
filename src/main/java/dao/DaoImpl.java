9d9670d8ea2691e696294bbfc1edaf7e8b1423da
steve desire <stevedesiredev@gmail.com>
2023-08-18 22:25:56 +0200

    first commit ioc_di spring version with dao metier and presentation layer created

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,14 @@
+package dao;
+
+public class DaoImpl implements IDao{
+
+    @Override
+    public double getData() {
+
+        //connect to database to get data
+        //here we just use a attribute as example for data
+        System.out.println("version base de données");
+        double temp=Math.random()*40;
+        return temp;
+    }
+}
